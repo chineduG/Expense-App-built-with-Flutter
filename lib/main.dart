5c77bea12dfa1f1156f93166d633377eecf75961
ChineduG <investorgbriel@gmail.com>
2023-01-18 11:09:13 +0100

    dateTime Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class _ExpenseAppState extends State<ExpenseApp> {
   // Tramsaction
   final List<Transaction> transactions = [
     Transaction(
-        id: 't1', title: 'New Shoes', amount: 67.00, date: DateTime.now()),
+        id: 't1', title: 'New Shoes', amount: 67.99, date: DateTime.now()),
     Transaction(
-        id: 't2', title: 'New Bags', amount: 35.00, date: DateTime.now())
+        id: 't2', title: 'New Bags', amount: 35.89, date: DateTime.now())
   ];
 
   @override
@@ -46,21 +46,40 @@ class _ExpenseAppState extends State<ExpenseApp> {
         children: [
           Container(
             width: double.infinity,
-            child: Card(
-                child: const Text('CHART'), elevation: 5, color: Colors.blue),
+            child: const Card(
+                child: Text('CHART'), elevation: 5, color: Colors.blue),
           ),
           Column(
             children: transactions.map((tx) {
               return Card(
                   child: Row(children: [
-                Container(child: Text(tx.amount.toString())),
-                Column(children: [
-                  Text(tx.title),
-                  Text(tx.date.toString()),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  // ignore: sort_child_properties_last
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                ),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(tx.title,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text(
+                    tx.date.toString(),
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 ])
               ]));
             }).toList(),
-          )
+          ),
         ],
       ),
     );
