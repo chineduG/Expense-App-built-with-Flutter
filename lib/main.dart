2466a61dae5af9c81d82906f32d1c5172dc49d20
ChineduG <investorgbriel@gmail.com>
2023-01-18 14:56:58 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,6 @@ class ExpenseApp extends StatefulWidget {
 
 class _ExpenseAppState extends State<ExpenseApp> {
 
-  // Transaction
-  final List<Transaction> transactions = [
-   
-  ];
-
   // TextController
 
    final titleControler = TextEditingController();
@@ -79,37 +74,7 @@ class _ExpenseAppState extends State<ExpenseApp> {
               ]),
             ),
           ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                  child: Row(children: [
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  // ignore: sort_child_properties_last
-                  child: Text(
-                    '\$${tx.amount}',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
-                ),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Text(tx.title,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 16)),
-                  Text(
-                    DateFormat.yMMMd().format(tx.date),
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ])
-              ]));
-            }).toList(),
-          ),
+         
         ],
       ),
     );
--- lib/widgets/trasaction_list.dart
+++ lib/widgets/trasaction_list.dart
@@ -1,5 +1,6 @@
 import 'package:build101/modles/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatefulWidget {
   const TransactionList({super.key});
@@ -9,13 +10,46 @@ class TransactionList extends StatefulWidget {
 }
 
 class _TransactionListState extends State<TransactionList> {
+
+  //! Predefined list of transactions.
   final List<Transaction> _userTransactions = [
     Transaction(
         id: 't1', title: 'New Shoes', amount: 67.99, date: DateTime.now()),
     Transaction(
         id: 't2', title: 'New Bags', amount: 35.89, date: DateTime.now())
   ];
+
+
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: _userTransactions.map((tx) {
+        return Card(
+            child: Row(children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            // ignore: sort_child_properties_last
+            child: Text(
+              '\$${tx.amount}',
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.purple),
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple, width: 2)),
+          ),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(tx.title,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+            Text(
+              DateFormat.yMMMd().format(tx.date),
+              style: TextStyle(color: Colors.grey),
+            ),
+          ])
+        ]));
+      }).toList(),
+    );
   }
 }
